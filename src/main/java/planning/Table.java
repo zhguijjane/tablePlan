1db0ad9c61b111a39b1d08a3eb37ed13156e5b29
Zouhair Guijjane <zh.guijjane@gmail.com>
2018-06-27 13:07:47 +0200

    Refacto(): Orders into Table

--- src/main/java/planning/Table.java
+++ src/main/java/planning/Table.java
@@ -1,16 +1,24 @@
 package planning;
 
-import java.util.*;
+import lombok.Value;
+
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-class Table {
+@Value
+public class Table {
 
     private static final int MAX_ORDER_PER_TABLE = 10;
+    private List<Order> orders;
 
-    public Map<MainMeal, Integer> order(List<Order> orders) {
+    public Map<MainMeal, Integer> countMainMeals() {
 
-        if (orders.size() > MAX_ORDER_PER_TABLE)
+        if (orders.size() > MAX_ORDER_PER_TABLE) {
             throw new IllegalArgumentException("You cannot have more than 10 orders for 1 table.");
+        }
+
         return orders.stream().collect(Collectors.groupingBy(Order::getMainMealChoice, Collectors.summingInt(x -> 1)));
     }
+
 }
--- src/test/java/planning/TableTest.java
+++ src/test/java/planning/TableTest.java
@@ -1,6 +1,5 @@
 package planning;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
@@ -10,42 +9,41 @@ import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 
 public class TableTest {
 
-    private Table table;
-
-    @Before
-    public void setUp() {
-        table = new Table();
-    }
 
     @Test
     public void should_return_1_meat_and_0_fish() {
         Order firstOrder = new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN);
-
         List<Order> orders = Collections.singletonList(firstOrder);
+        Table table = new Table(orders);
 
-        HashMap<MainMeal, Integer> mainMeals = new HashMap<>();
-        mainMeals.put(MainMeal.MEAT, 1);
 
-        assertThat(table.order(orders)).isEqualTo(mainMeals);
+        Map<MainMeal, Integer> actualMainMeals = table.countMainMeals();
+
+
+        Map<MainMeal, Integer> expectedMainMeals = new HashMap<>();
+        expectedMainMeals.put(MainMeal.MEAT, 1);
+        assertThat(actualMainMeals).isEqualTo(expectedMainMeals);
     }
 
     @Test
     public void should_return_1_meat_and_1_fish() {
         Order firstOrder = new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN);
         Order secondOrder = new Order(StarterMeal.SALAD, MainMeal.FISH, DessertMeal.FLAN);
-
         List<Order> orders = Arrays.asList(firstOrder, secondOrder);
+        Table table = new Table(orders);
 
-        HashMap<MainMeal, Integer> mainMeals = new HashMap<>();
-        mainMeals.put(MainMeal.MEAT, 1);
-        mainMeals.put(MainMeal.FISH, 1);
 
-        assertThat(table.order(orders)).isEqualTo(mainMeals);
+        Map<MainMeal, Integer> actualMainMeals = table.countMainMeals();
+
+
+        Map<MainMeal, Integer> expectedMainMeals = new HashMap<>();
+        expectedMainMeals.put(MainMeal.MEAT, 1);
+        expectedMainMeals.put(MainMeal.FISH, 1);
+        assertThat(actualMainMeals).isEqualTo(expectedMainMeals);
     }
 
     @Test
     public void should_return_3_meat_and_7_fish() {
-
         List<Order> orders = new ArrayList<>();
         Collections.addAll(orders, new Order(StarterMeal.SALAD, MainMeal.FISH, DessertMeal.FLAN),
                 new Order(StarterMeal.SALAD, MainMeal.FISH, DessertMeal.FLAN),
@@ -57,17 +55,20 @@ public class TableTest {
                 new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN),
                 new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN),
                 new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN));
+        Table table = new Table(orders);
 
-        HashMap<MainMeal, Integer> mainMeals = new HashMap<>();
-        mainMeals.put(MainMeal.MEAT, 3);
-        mainMeals.put(MainMeal.FISH, 7);
 
-        assertThat(table.order(orders)).isEqualTo(mainMeals);
+        Map<MainMeal, Integer> actualMainMeals = table.countMainMeals();
+
+
+        Map<MainMeal, Integer> expectedMainMeals = new HashMap<>();
+        expectedMainMeals.put(MainMeal.MEAT, 3);
+        expectedMainMeals.put(MainMeal.FISH, 7);
+        assertThat(actualMainMeals).isEqualTo(expectedMainMeals);
     }
 
     @Test
     public void should_return_exception_for_more_than_10_orders_per_table() {
-
         List<Order> orders = new ArrayList<>();
         Collections.addAll(orders,
                 new Order(StarterMeal.SALAD, MainMeal.FISH, DessertMeal.FLAN),
@@ -83,17 +84,24 @@ public class TableTest {
                 new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN),
                 new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN),
                 new Order(StarterMeal.SALAD, MainMeal.MEAT, DessertMeal.FLAN));
+        Table table = new Table(orders);
 
-        assertThatThrownBy(() -> table.order(orders)).isInstanceOf(IllegalArgumentException.class)
+
+        assertThatThrownBy(table::countMainMeals)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("You cannot have more than 10 orders for 1 table.");
     }
 
     @Test
     public void should_return_no_meal_for_no_order() {
         List<Order> orders = Collections.emptyList();
+        Table table = new Table(orders);
 
-        Map<Object, Object> mainMeals = Collections.emptyMap();
 
-        assertThat(table.order(orders)).isEqualTo(mainMeals);
+        Map<MainMeal, Integer> actualMainMeals = table.countMainMeals();
+
+
+        Map<Object, Object> expectedMainMeals = Collections.emptyMap();
+        assertThat(actualMainMeals).isEqualTo(expectedMainMeals);
     }
 }
